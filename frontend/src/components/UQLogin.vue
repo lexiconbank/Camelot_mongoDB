c09c2d7dfea9499a2668f4102aa7eff5d2f1e31d
Michael Merin <mhanel.merin14@gmail.com>
2021-02-26 23:14:49 +0800

    FrontEnd Design

--- frontend/src/components/UQLogin.vue
+++ frontend/src/components/UQLogin.vue
@@ -3,18 +3,44 @@
 
 	    <!-- content -->
 	    <div class='content text-left'>
-	    	<q-form @submit="findUser()">
-		     	<div class="field">
-		       		<label>Username</label>
-		       		<div><q-input v-model="form_data.email" placeholder="Enter Email" outlined dense></q-input></div>
-		       </div>
-
-		        <div class="field q-mt-md">
-		       		<label>Password</label>
-		       		<div><q-input v-model="form_data.password" type="password" placeholder="Enter Password" outlined dense></q-input></div>
-		       </div>
-		       <div class="q-mt-lg"><q-btn type="submit" color="primary" unelevated class="full-width">Login</q-btn></div>
-		       <div class="q-mt-sm"><q-btn type="button" v-close-popup color="primary" outline class="full-width">Cancel</q-btn></div>
+	    	<q-form @submit="loginUser()">
+                <div class="field">
+                    <div>
+                        <q-input value="https://www.camelot.com" readonly outlined >
+                        <template v-slot:prepend>
+                            <q-icon name="lock" color="positive" />
+                            <div class="text-caption" style="color:#21BA45">secure</div>
+                        </template>
+                        </q-input>
+                    </div>
+                </div>
+                <div class="field q-mt-lg">
+                    <div><q-input v-model="form_data.email" placeholder="Enter Email" outlined ></q-input></div>
+                </div>
+                <div class="field q-mt-md">
+                    <div>
+                        <q-input outlined v-model="form_data.password" placeholder="Enter Password" :type="isPwd ? 'password' : 'text'" >
+                            <template v-slot:append>
+                                <q-icon
+                                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                                    class="cursor-pointer"
+                                    @click="isPwd = !isPwd"
+                                    
+                                />
+                            </template>
+                        </q-input>
+                    </div>
+                </div>
+                
+                <div class="q-mt-lg">
+                    <a href="" tabindex="0" class="my__hover q-btn inline full-width text-right text-caption q-btn-item non-selectable q-btn--flat q-btn--rectangle text-black q-focusable q-hoverable q-btn--no-uppercase">
+                      <div>Forgot password?</div>
+                    </a>
+                    <q-btn type="submit" color="primary" unelevated class="full-width q-mt-xs">Login</q-btn>
+                    <a  href="" tabindex="0" class="my__hover q-mt-sm q-btn inline full-width q-btn-item non-selectable q-btn--flat q-btn--rectangle text-black q-focusable q-hoverable q-btn--no-uppercase">
+                       <div>Create an account</div>
+                    </a>
+                 </div>
 	   		</q-form>
 	    </div>
     </div>
@@ -32,6 +58,7 @@ export default
             email: '',
             password: '',
         },
+        isPwd : true,
     }),
     mounted()
     {
@@ -39,7 +66,7 @@ export default
     },
     methods:
     {
-        async findUser()
+        async loginUser()
         {
             this.$q.loading.show();
             let login = await this.$_post(postLoginUser, this.form_data);
@@ -52,3 +79,9 @@ export default
     }
 }
 </script>
+
+<style scoped>
+    .my__hover:hover{
+        text-decoration: underline ;
+    }
+</style>
